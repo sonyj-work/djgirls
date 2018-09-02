814cb44d359b479449cf96b931d507617c0a0c4f
sonyj-work <sonyj97@gmail.com>
2018-09-03 01:58:39 +0900

    added a login function with a button, a logout function with a hyperlink

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include, re_path
+from django.contrib import admin
+
+
 from . import views
 
 urlpatterns = [
--- djgirls/urls.py
+++ djgirls/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.views.generic.base import TemplateView
+
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
 ]
